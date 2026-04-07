ffa9eb0317fee72e371ed609f0783bd7433ea420
Senuka Chandunu <senukaacc@gmail.com>
2026-04-07 17:15:29 +0530

    chore: fix deprecated withOpacity usage

--- lib/widgets/my_route_tab.dart
+++ lib/widgets/my_route_tab.dart
@@ -219,7 +219,7 @@ class _ActiveJobCardState extends State<ActiveJobCard> {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.08),
+            color: Colors.black.withValues(alpha: 0.08),
             blurRadius: 15,
             spreadRadius: 2,
             offset: const Offset(0, 5),
